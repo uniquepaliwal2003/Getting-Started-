ec7127c86fcd2f7ff51813504f77c871e7626b83
Unique paliwal <zantro20@gmail.com>
2023-01-06 22:30:07 +0530

    this is to greet unique

--- something.cpp
+++ something.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"first file to change in the git lesson";
+    cout<<"first file to change in the git lesson"<<endl;
+    cout<<"Saying hi to unique paliwal";
     return 0;
 }
